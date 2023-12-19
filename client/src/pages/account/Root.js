a36191a4f3c7a866cd82e05bf2669ecc891a1b5d
keonepulse <dev00engine@outlook.com>
2023-12-18 17:49:15 -0800

    implement sidebar

--- client/src/pages/account/Root.js
+++ client/src/pages/account/Root.js
@@ -1,34 +1,55 @@
 import { Outlet, NavLink } from 'react-router-dom';
+import { GoStack } from 'react-icons/go';
+import { GrTransaction } from 'react-icons/gr';
+
+// TODO: create a separate file for this component
+const SideBarNavItem = ({ label, path, icon }) => {
+  return (
+    <NavLink to={path}>
+      {({ isActive }) => (
+        <li
+          className={`flex flex-row items-center gap-3.5 px-5 py-2.5 text-sm
+            font-light text-white subpixel-antialiased transition-all duration-200 ${
+              isActive ? 'bg-zinc-900 text-white' : 'text-zinc-300'
+            }`}>
+          {icon}
+          {label}
+        </li>
+      )}
+    </NavLink>
+  );
+};
+
+// TODO: create a separate file for this component
+const SideBar = () => {
+  return (
+    <aside className='left-0 h-screen w-fit bg-zinc-700'>
+      <nav>
+        <ul>
+          <SideBarNavItem
+            label='Accounts'
+            path='/account/accounts'
+            icon={<GoStack />}
+          />
+          <SideBarNavItem
+            label='Transactions'
+            path='/account/transactions'
+            icon={<GrTransaction />}
+          />
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
 const Root = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to='/account/accounts'
-              className={({ isActive, isPending }) =>
-                isPending ? 'pending' : isActive ? 'text-purple-900' : ''
-              }>
-              Accounts
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/account/transactions'
-              className={({ isActive, isPending }) =>
-                isPending ? 'pending' : isActive ? 'text-purple-900' : ''
-              }>
-              Transactions
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <main>
+    <div className='flex'>
+      <SideBar />
+      <main className='w-full bg-slate-50'>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
