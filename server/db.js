3430c87e2b99ce92e6c4c2008a426a989ade35ab
keonepulse <dev00engine@outlook.com>
2023-12-26 20:54:48 -0800

    prevent mongodb connection when in demo mode & remove unused file

--- server/db.js
+++ server/db.js
@@ -1,8 +1,14 @@
+const { DEMO_MODE } = require('./utils/constants');
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose
-    .connect(process.env.MONGODB_CLUSTER_URL)
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+  console.log('DEMO_MODE:', DEMO_MODE ? 'ON' : 'OFF');
+  if (!DEMO_MODE) {
+    mongoose
+      .connect(process.env.MONGODB_CLUSTER_URL)
+      .then(() => console.log('Connected to MongoDB...'))
+      .catch(() => console.error('Could not connect to MongoDB...'));
+  } else {
+    console.log('Not connected to MongoDB, mock data will be used instead...');
+  }
 };
