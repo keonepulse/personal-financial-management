17c8fc0534308a5d5938fedbf80de317cc8cf0e6
keonepulse <dev00engine@outlook.com>
2023-12-27 11:46:22 -0800

    update styling on Card & Header components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = ({ children }) => {
   console.log('render - Header');
   return (
-    <header className='sticky top-0 bg-white py-2.5 text-center shadow-md'>
+    <header className='sticky top-0 z-10 bg-white py-2.5 text-center shadow-lg'>
       {children}
     </header>
   );
--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -1,5 +1,11 @@
 const Card = ({ children, className }) => {
-  return <div className={`${className} bg-white shadow-md`}>{children}</div>;
+  return (
+    <div
+      className={`${className} overflow-hidden rounded-lg bg-white shadow-lg
+        ring-1 ring-slate-900/5`}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
