2b74811f10356c72bc4cec5846eb0dfcb828e961
keonepulse <dev00engine@outlook.com>
2024-02-16 20:10:10 -0800

    add server helper functions w/ tests

--- server/tests/helpers.test.js
+++ server/tests/helpers.test.js
@@ -1,4 +1,12 @@
-const { getRandomId, getRandomNumber } = require('../utils/helpers');
+const {
+  getRandomId,
+  getRandomNumber,
+  getLastFourteenMonths,
+  getRandomDayOfMonth,
+  mockAccountLink,
+  mockAccountBalance,
+  mockTransaction
+} = require('../utils/helpers');
 
 describe('getRandomId', () => {
   it('returns a random string of length 8', () => {
@@ -25,5 +33,97 @@ describe('getRandomNumber', () => {
 
   it('throws an error if min is greater than max', () => {
     expect(() => getRandomNumber(10, 1)).toThrow();
+  });
+});
+
+describe('getLastFourteenMonths', () => {
+  it('returns an array of the last twelve months in YYYY-MM format', () => {
+    const months = getLastFourteenMonths();
+    expect(months.length).toBe(14);
+    months.forEach(month => {
+      expect(month).toMatch(/^\d{4}-\d{2}$/);
+    });
+  });
+});
+
+describe('getRandomDayOfMonth', () => {
+  it('returns a string of length 2', () => {
+    const day = getRandomDayOfMonth();
+    expect(typeof day).toBe('string');
+    expect(day.length).toBe(2);
+  });
+});
+
+describe('mockAccountLink', () => {
+  it('returns an account link object with empty balance and transaction data', () => {
+    const name = 'plaid';
+    const link = mockAccountLink(name);
+
+    expect(link).toHaveProperty('_id');
+    expect(link).toHaveProperty('name', name);
+    expect(link).toHaveProperty('access_token');
+    expect(link).toHaveProperty('item_id');
+    expect(link).toHaveProperty('created_at');
+    expect(link).toHaveProperty('balance');
+    expect(link.balance).toHaveProperty('accounts');
+    expect(link.balance).toHaveProperty('updated_at');
+    expect(link).toHaveProperty('transactions');
+    expect(link.transactions).toHaveProperty('data');
+    expect(link.transactions).toHaveProperty('updated_at');
+    expect(link.transactions).toHaveProperty('cursor');
+  });
+});
+
+describe('mockAccountBalance', () => {
+  it('returns an account balance object with random account ID and mask', () => {
+    const available = 1000;
+    const current = 500;
+    const limit = 1500;
+    const balance = {
+      balances: {
+        available: available,
+        current: current,
+        limit: limit
+      },
+      name: 'checking',
+      official_name: 'Chase Checking',
+      subtype: 'checking',
+      type: 'depository'
+    };
+    const accountBalance = mockAccountBalance(balance);
+
+    expect(accountBalance).toHaveProperty('account_id');
+    expect(accountBalance).toHaveProperty('balances');
+    expect(accountBalance.balances).toHaveProperty('iso_currency_code', 'USD');
+    expect(accountBalance.balances).toHaveProperty('available', available);
+    expect(accountBalance.balances).toHaveProperty('current', current);
+    expect(accountBalance.balances).toHaveProperty('limit', limit);
+    expect(accountBalance).toHaveProperty('mask');
+  });
+});
+
+describe('mockTransaction', () => {
+  it('returns a transaction object with random account ID and transaction ID', () => {
+    const transaction = {
+      date: '2024-02-01',
+      category: ['Food and Drink', 'Restaurants'],
+      name: 'Chipotle',
+      amount: 25
+    };
+    const mock = mockTransaction(transaction);
+
+    expect(mock).toHaveProperty('_id');
+    expect(mock).toHaveProperty('location');
+    expect(mock).toHaveProperty('payment_channel');
+    expect(mock).toHaveProperty('transaction_type');
+    expect(mock).toHaveProperty('iso_currency_code', 'USD');
+    expect(mock).toHaveProperty('merchant_entity_id');
+    expect(mock).toHaveProperty('website');
+    expect(mock).toHaveProperty('note');
+    expect(mock).toHaveProperty('category', transaction.category);
+    expect(mock).toHaveProperty('amount', transaction.amount);
+    expect(mock).toHaveProperty('date', transaction.date);
+    expect(mock).toHaveProperty('name', transaction.name);
+    expect(mock).toHaveProperty('pending', false);
   });
 });
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,4 +1,4 @@
-// Generates a random string of the given length.
+// Generates a random alphanumeric string of the given length.
 // If no length is provided, the default length is 8.
 //
 // @param length: length of the random string
@@ -19,7 +19,128 @@ const getRandomNumber = (min, max) => {
   return Math.random() * (max - min) + min;
 };
 
+// For simplicity, this will simply return a random number between 1 and 28.
+// The result is left padded with a 0 if it is less than 10.
+//
+// @return string: random day of the month
+const getRandomDayOfMonth = () => {
+  const day = Math.floor(getRandomNumber(1, 29));
+  return day < 10 ? `0${day}` : day.toString();
+};
+
+// @return array: array of strings representing the current month and the
+//         previous thirteen months in YYYY-MM format
+const getLastFourteenMonths = () => {
+  const months = [];
+  for (let i = 0; i < 14; i++) {
+    // Start from 0 to include the current month
+    const date = new Date();
+    date.setMonth(date.getMonth() - i);
+    months.push(date.toISOString().slice(0, 7));
+  }
+  return months;
+};
+
+// @param name: name of the account link
+// @return object: account link object with empty balance and transaction data
+const mockAccountLink = name => {
+  return {
+    _id: getRandomId(),
+    name: name,
+    access_token: `access-demo-${getRandomId()}`,
+    item_id: getRandomId(),
+    created_at: new Date(),
+    balance: {
+      accounts: [],
+      updated_at: null
+    },
+    transactions: {
+      data: [],
+      updated_at: null,
+      cursor: null
+    }
+  };
+};
+
+// @param balance: object containing balance data
+// @return object: account balance object with random account ID and mask
+const mockAccountBalance = balance => {
+  return {
+    ...balance,
+    account_id: getRandomId(),
+    balances: {
+      iso_currency_code: 'USD',
+      ...balance.balances
+    },
+    mask: parseInt(getRandomNumber(1000, 9999))
+  };
+};
+
+// @param transaction: object containing transaction data
+// @return object: transaction object with random IDs
+const mockTransaction = transaction => {
+  return {
+    _id: getRandomId(),
+    account_owner: null,
+    amount: null,
+    authorized_date: null,
+    authorized_datetime: null,
+    category: null,
+    category_id: null,
+    check_number: null,
+    counterparties: [],
+    date: null,
+    datetime: null,
+    iso_currency_code: 'USD',
+    location: {
+      address: null,
+      city: null,
+      country: null,
+      lat: null,
+      lon: null,
+      postal_code: null,
+      region: null,
+      store_number: getRandomNumber(1000, 9999)
+    },
+    logo_url: null,
+    merchant_entity_id: getRandomId(),
+    merchant_name: null,
+    name: null,
+    payment_channel: null,
+    payment_meta: {
+      by_order_of: null,
+      payee: null,
+      payer: null,
+      payment_method: null,
+      payment_processor: null,
+      ppd_id: null,
+      reason: null,
+      reference_number: null
+    },
+    pending: false,
+    pending_transaction_id: getRandomId(),
+    personal_finance_category: {
+      confidence_level: null,
+      detailed: null,
+      primary: null
+    },
+    personal_finance_category_icon_url: null,
+    transaction_code: null,
+    transaction_id: getRandomId(),
+    transaction_type: null,
+    unofficial_currency_code: null,
+    website: 'https://github.com/karamvirr',
+    note: null,
+    ...transaction
+  };
+};
+
 module.exports = {
   getRandomId,
-  getRandomNumber
+  getRandomNumber,
+  getRandomDayOfMonth,
+  getLastFourteenMonths,
+  mockAccountLink,
+  mockAccountBalance,
+  mockTransaction
 };
